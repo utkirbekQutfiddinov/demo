029d039c27ff247c867856c60cca98f324e5d21d
utkirbek <utkirbek@mail.ru>
2024-01-29 01:15:44 +0500

    test controller

---
+++ src/main/java/uz/utkirbek/controller/UserController.java
@@ -0,0 +1,13 @@
+package uz.utkirbek.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "salom";
+    }
+}
