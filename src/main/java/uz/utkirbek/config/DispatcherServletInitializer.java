b89ee09cf755673e103430eeb9d8449d4dad4641
utkirbek <utkirbek@mail.ru>
2024-01-29 00:26:04 +0500

    java based configuration

---
+++ src/main/java/uz/utkirbek/config/DispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package uz.utkirbek.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{HibernateConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebMvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
