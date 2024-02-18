3264e5c8135b6527089cb342f4238af893d4c39a
utkirbek <utkirbek@mail.ru>
2024-02-18 13:21:05 +0500

    new branch for spring boot app

---
+++ src/main/java/uz/utkirbek/GymCrmApplication.java
@@ -0,0 +1,11 @@
+package uz.utkirbek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCrmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GymCrmApplication.class, args);
+    }
+}
