6393d361fd48a2cba95300a293b5f813d3a59c32
utkirbek <utkirbek@mail.ru>
2024-02-24 13:13:24 +0500

    created two profiles: dev and prod

--- src/main/java/uz/utkirbek/config/HibernateConfig.java
+++ src/main/java/uz/utkirbek/config/HibernateConfig.java
@@ -5,13 +5,22 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class HibernateConfig {
 
     @Bean
+    @Profile("prod")
     public EntityManager getEntityManager() {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("epamTask");
+        return entityManagerFactory.createEntityManager();
+    }
+
+    @Bean
+    @Profile("dev")
+    public EntityManager getEntityManagerDev() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("epamTaskDev");
         return entityManagerFactory.createEntityManager();
     }
 }
