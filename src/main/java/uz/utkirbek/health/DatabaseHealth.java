f63ebdad0fa25e839915b733e630216c2db8c221
utkirbek <utkirbek@mail.ru>
2024-02-28 22:45:42 +0500

    logging and properties have been fixed

--- src/main/java/uz/utkirbek/health/DatabaseHealth.java
+++ src/main/java/uz/utkirbek/health/DatabaseHealth.java
@@ -6,13 +6,15 @@ import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 @Component
-@PropertySource("classpath:database.properties")
 public class DatabaseHealth implements HealthIndicator {
+    private static final Logger logger = Logger.getLogger(DatabaseHealth.class.getName());
 
     @Value("${db.url}")
     private String jdbcUrl;
@@ -28,6 +30,7 @@ public class DatabaseHealth implements HealthIndicator {
         try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
             return Health.up().withDetail("Database status", "Database is available").build();
         } catch (SQLException e) {
+            logger.warning("Exception on: " + e.getMessage());
             return Health.down().withDetail("Database status", "Database is not available").build();
         }
     }
--- src/main/java/uz/utkirbek/health/ServiceHealth.java
+++ src/main/java/uz/utkirbek/health/ServiceHealth.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Component;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.logging.Logger;
 
 @Component
 public class ServiceHealth implements HealthIndicator {
+    private static final Logger logger = Logger.getLogger(ServiceHealth.class.getName());
 
     private static final String EXTERNAL_SERVICE_URL = "http://localhost:8082/serviceHealth";
+    private static final String REQUEST_METHOD_NAME = "GET";
+
 
     @Override
     public Health health() {
@@ -27,10 +31,11 @@ public class ServiceHealth implements HealthIndicator {
         try {
             URL url = new URL(EXTERNAL_SERVICE_URL);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+            connection.setRequestMethod(REQUEST_METHOD_NAME);
             int responseCode = connection.getResponseCode();
             return responseCode == HttpURLConnection.HTTP_OK;
         } catch (Exception e) {
+            logger.warning("Exception on: " + e.getMessage());
             return false;
         }
     }
